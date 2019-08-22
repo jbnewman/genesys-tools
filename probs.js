5f9a350f2fb2ea7ddeaeca7c4b1b0f56d63bfc00
Brandon Newman <brandon.newman@gmail.com>
2019-08-21 19:05:17 -0500

    update

---
+++ probs.js
@@ -0,0 +1,17 @@
+let probs = [];
+/*
+Good Dice
+A = ability
+B = boost
+P = proficiency
+
+D = difficulty
+S = setback
+C = challenge
+*/
+probs["A"] = [];
+probs["A"]["D"] = "34%";
+probs["A"]["C"] = "26%";
+probs["A"]["DD"] = "23%";
+probs["A"]["CD"] = "18%";
+probs["A"]["CC"] = "13%";
