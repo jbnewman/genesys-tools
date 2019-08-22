36d6ad0e778e04c530bf66c7b4587e248bce7937
Brandon Newman <brandon.newman@gmail.com>
2019-08-21 20:17:19 -0500

    update

--- probs.js
+++ probs.js
@@ -1,4 +1,3 @@
-let probs = [];
 /*
 Good Dice
 A = ability
@@ -9,9 +8,31 @@ D = difficulty
 S = setback
 C = challenge
 */
-probs["A"] = [];
-probs["A"]["D"] = "34%";
-probs["A"]["C"] = "26%";
-probs["A"]["DD"] = "23%";
-probs["A"]["CD"] = "18%";
-probs["A"]["CC"] = "13%";
+var probs = [];
+probs["B"] = [0, 0, 1, 1, 0, 0];
+probs["S"] = [0, 0, -1, -1, 0, 0];
+probs["A"] = [0, 1, 1, 2, 0, 0, 1, 0];
+probs["D"] = [0, -1, -2, 0, 0, 0, 0, -1];
+probs["P"] = [0, 1, 1, 2, 2, 0, 1, 1, 1, 0, 0, 1];
+probs["C"] = [0, -1, -1, -2, -2, 0, 0, -1, -1, 0, 0, -1];
+
+function roll_dice(die) {
+  return probs[die][Math.floor(Math.random() * probs[die].length)];
+}
+function eval_roll(str) {
+  var i = str.length;
+  var res = 0;
+  while (i--) {
+    res += roll_dice(str.charAt(i));
+  }
+  return res > 0;
+}
+function get_prob(str, rolls = 10000) {
+  var suc = 0;
+  for (i = 0; i < rolls; i++) {
+    if (eval_roll(str)) {
+      suc++;
+    }
+  }
+  return parseInt((parseFloat(suc) / parseFloat(rolls)) * 100);
+}
